996d496425fd94a7bd88378b3fa95fbc221d3597
sivahanumanraju-petha <sivahanumanraju.petha@thezennialpro.com>
2025-09-05 15:40:15 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,24 @@
+
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+from app.config import JWT_SECRET, JWT_ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from typing import Optional
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain: str, hashed: str) -> bool:
+    return pwd_context.verify(plain, hashed)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    token = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return token
+
+def decode_token(token: str):
+    return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
---
+++ main.py
@@ -0,0 +1,23 @@
+
+import uvicorn
+from fastapi import FastAPI
+from app.routes import auth_routes, donor_routes, request_routes
+from app.database import init_db
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "LifeLink is running!"}
+
+
+app.include_router(auth_routes.router)
+app.include_router(donor_routes.router)
+app.include_router(request_routes.router)
+
+@app.on_event("startup")
+async def startup_event():
+    await init_db()
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
